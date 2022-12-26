0c600e4174697027c9cf99169ed786c6ac14ce9b
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-12-26 16:04:11 +0500

    get item

--- db.go
+++ db.go
@@ -5,8 +5,11 @@ import (
 
 	"log"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 type Todo struct {
@@ -27,4 +30,37 @@ func init() {
 	}
 
 	db = *dynamodb.NewFromConfig(sdkConfig)
+}
+
+func getItem(ctx context.Context, id string) (*Todo, error) {
+	key, err := attributevalue.Marshal(id)
+	if err != nil {
+		return nil, err
+	}
+
+	input := &dynamodb.GetItemInput{
+		TableName: aws.String(TableName),
+		Key: map[string]types.AttributeValue{
+			"id": key,
+		},
+	}
+
+	log.Printf("Calling Dynamodb with input: %v", input)
+	result, err := db.GetItem(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Executed GetItem DynamoDb successfully. Result: %#v", result)
+
+	if result.Item == nil {
+		return nil, nil
+	}
+
+	todo := new(Todo)
+	err = attributevalue.UnmarshalMap(result.Item, todo)
+	if err != nil {
+		return nil, err
+	}
+
+	return todo, nil
 }

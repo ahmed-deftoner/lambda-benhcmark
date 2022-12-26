ad7fbca4d6af1288c0d4f26433a17125db1035ab
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-12-26 15:46:58 +0500

    todo struct

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type Todo struct {
+	Id          string `json:"id" dynamodbav:"id"`
+	Name        string `json:"name" dynamodbav:"name"`
+	Description string `json:"description" dynamodbav:"description"`
+	Status      bool   `json:"status" dynamodbav:"status"`
+}
+
 func main() {
 	lambda.Start(router)
 }

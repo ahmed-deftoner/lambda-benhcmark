3ed380be6dcd2cfa41e54c9208b8fa639aae4e50
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-12-26 16:08:06 +0500

    put item

--- db.go
+++ db.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/google/uuid"
 )
 
 type Todo struct {
@@ -63,4 +64,40 @@ func getItem(ctx context.Context, id string) (*Todo, error) {
 	}
 
 	return todo, nil
+}
+
+type CreateTodo struct {
+	Name        string `json:"name" validate:"required"`
+	Description string `json:"description" validate:"required"`
+}
+
+func insertItem(ctx context.Context, createTodo CreateTodo) (*Todo, error) {
+	todo := Todo{
+		Name:        createTodo.Name,
+		Description: createTodo.Description,
+		Status:      false,
+		Id:          uuid.NewString(),
+	}
+
+	item, err := attributevalue.MarshalMap(todo)
+	if err != nil {
+		return nil, err
+	}
+
+	input := &dynamodb.PutItemInput{
+		TableName: aws.String(TableName),
+		Item:      item,
+	}
+
+	res, err := db.PutItem(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
+	err = attributevalue.UnmarshalMap(res.Attributes, &todo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &todo, nil
 }

3b2805d73e4d6fc21099e21dc7513f91ccd78c47
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-12-26 16:10:53 +0500

    list items

--- db.go
+++ db.go
@@ -207,3 +207,34 @@ func updateItem(ctx context.Context, id string, updateTodo UpdateTodo) (*Todo, e
 
 	return todo, nil
 }
+
+func listItems(ctx context.Context) ([]Todo, error) {
+	todos := make([]Todo, 0)
+	var token map[string]types.AttributeValue
+
+	for {
+		input := &dynamodb.ScanInput{
+			TableName:         aws.String(TableName),
+			ExclusiveStartKey: token,
+		}
+
+		result, err := db.Scan(ctx, input)
+		if err != nil {
+			return nil, err
+		}
+
+		var fetchedTodos []Todo
+		err = attributevalue.UnmarshalListOfMaps(result.Items, &fetchedTodos)
+		if err != nil {
+			return nil, err
+		}
+
+		todos = append(todos, fetchedTodos...)
+		token = result.LastEvaluatedKey
+		if token == nil {
+			break
+		}
+	}
+
+	return todos, nil
+}
